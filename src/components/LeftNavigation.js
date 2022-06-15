55a3428f1899f2384ac23e35204ce450155e9286
Sneha <snhthakare@gmail.com>
2022-06-15 15:26:35 +0300

    Adds left button group

---
+++ src/components/LeftNavigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import nav from './../assets/images/left_nav_group.svg'
+import './../assets/css/LeftNavigation.css'
+
+const LeftNavigation = () => {
+    return (
+        <div className='leftNav'><img src={nav} alt="left nav" /></div>
+    )
+}
+
+export default LeftNavigation
