79518827b08ad869580f66178e9b8a5f70df20b7
Sneha <snhthakare@gmail.com>
2022-06-15 23:51:08 +0300

    Adds footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './../assets/css/Footer.css'
+
+const Footer = () => {
+    return (
+        <div><span className='footer'>Terms&Conditions | Privacy policy</span></div>
+    )
+}
+
+export default Footer
