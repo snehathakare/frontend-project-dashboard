b30dbe3f3069764945fc2626e4e5eb3864393fd2
Sneha <snhthakare@gmail.com>
2022-08-15 16:54:58 +0530

    Updates Top nav

--- src/components/Navigation/TopNavigation.js
+++ src/components/Navigation/TopNavigation.js
@@ -1,19 +1,21 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import './../../assets/css/TopNavigation.css'
 import logo from './../../assets/images/logo.svg';
 import menu from './../../assets/images/menu.svg';
 
 const TopNavigation = () => {
     return (
-        <Fragment>
-            <img className='topnav-logo' src={logo} alt="logo" />
-            <img className='topnav-menu' src={menu} alt="menu" />
+        <div className='top-nav-container'>
+            <div className='top-nav-left'>
+                <img className='topnav-logo' src={logo} alt="logo" />
+                <img className='topnav-menu' src={menu} alt="menu" />
+            </div>
             <div className='topnav-right'>
                 <div className='bg_user' />
                 <div className='user_initial'><span>JD</span></div>
                 <div className="username">username</div>
             </div>
-        </Fragment>
+        </div>
     )
 }
 
